dfcb48e488fbb6b59e17074f6ac2fe083561aacc
varshith707 <VARSHITH707@users.noreply.github.com>
2026-08-29 00:19:43 +0530

    Bind server to 0.0.0.0 and PORT env var for hosting on Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 from debate_engine import run_debate
 
@@ -83,4 +84,4 @@ If a debate fails, please retry after some time.
     )
 
 
-app.launch()
+app.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
